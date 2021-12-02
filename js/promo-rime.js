78e41fafd84422bd29295635454d02839650fc2f
RB-bit <rodrigobossi097@gmail.com>
2021-12-02 12:07:47 -0300

    add winestores rime

--- js/promo-rime.js
+++ js/promo-rime.js
@@ -44,6 +44,51 @@ let vinotecasRime = [
     bodega: "amalaya",
     thumbnailUrl: "img/vinotecas/amalaya/elcamino.png"
   },
+  {
+    id: 10,
+    vinoteca: "Langosco",
+    direccion: "Córdoba 1725",
+    ir: "https://g.page/LANGOSCO-VINOS?share",
+    barrio: "Zona Centro",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/langosco"
+  },
+  {
+    id: 8,
+    vinoteca: "Vinoteca Ruta 40",
+    direccion: "Buenos Aires 1490",
+    ir: "https://goo.gl/maps/2LLCRJQ8m7Z2bGoX9",
+    barrio: "Zona Centro",
+    bodega: "casarena",
+    thumbnailUrl: "img/vinotecas/casarena/vinotecaruta40.jpeg"
+  },
+  {
+    id: 6,
+    vinoteca: "La Barrica",
+    direccion: "Urquiza 1187",
+    ir: "https://g.page/LaBarricaVineria?share",
+    barrio: "Zona Centro",
+    bodega: "casarena",
+    thumbnailUrl: "img/vinotecas/casarena/labarrica.png"
+  },
+  {
+    id: 6,
+    vinoteca: "Club de Sabores",
+    direccion: "Buenos Aires 996",
+    ir: "https://goo.gl/maps/GEMaKzo568oEd7x68",
+    barrio: "Zona Centro",
+    bodega: "casarena",
+    thumbnailUrl: "img/vinotecas/casarena/random4"
+  },
+  {
+    id: 6,
+    vinoteca: "Casa Burns",
+    direccion: "Moreno 873 - Local 7",
+    ir: "https://goo.gl/maps/xvrLXBcwt6H7vPhM6",
+    barrio: "Zona Centro",
+    bodega: "casarena",
+    thumbnailUrl: "img/vinotecas/rime/random2.jpeg"
+  },
   // Zona macro Centro
   {
     id: 9,
@@ -72,6 +117,53 @@ let vinotecasRime = [
     bodega: "rime",
     thumbnailUrl: "img/vinotecas/amalaya/random4"
   },
+  {
+    id: 12,
+    vinoteca: "El Gran Tonel",
+    direccion: "Mendoza 3674",
+    ir: "https://goo.gl/maps/kxF32zWHB5p47T498",
+    barrio: "Zona Macro Centro",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/rime/elgrantonel.png"
+  },
+  {
+    id: 18,
+    vinoteca: "Mediterráneo",
+    direccion: "Cafferata 356",
+    ir: "https://goo.gl/maps/oAPTesEy1HATS8jZ7",
+    barrio: "Zona Macro Centro",
+    bodega: "canale",
+    thumbnailUrl: "img/vinotecas/canale/mediterraneo"
+  },
+  //Zona Sur
+  {
+    id: 45,
+    vinoteca: "Catas de Garaje",
+    direccion: "Arijón 1375",
+    ir: "https://g.page/catasdegarage?share",
+    barrio: "Zona Sur",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/catasdegaraje"
+  },
+  {
+    id: 20,
+    vinoteca: "Fiambrería Amenabar",
+    direccion: "Rueda 2165",
+    ir: "https://goo.gl/maps/JuyyaFftSMTZw8KF8",
+    barrio: "Zona Sur",
+    bodega: "canale",
+    thumbnailUrl: "img/vinotecas/canale/amenabar.png"
+  },
+  //Zona Norte
+  {
+    id: 20,
+    vinoteca: "Puro Vino",
+    direccion: "Baigorria 601",
+    ir: "https://goo.gl/maps/4cW4tiv5AatEAZ819",
+    barrio: "Zona Norte",
+    bodega: "canale",
+    thumbnailUrl: "img/vinotecas/rime/purovino.png"
+  },
   //Granadero Baigorria
   {
     id: 12,
@@ -81,6 +173,238 @@ let vinotecasRime = [
     barrio: "Zona Granadero Baigorria",
     bodega: "rime",
     thumbnailUrl: "img/vinotecas/rime/chateau.png"
+  },
+  ,
+  // Rafaela
+  {
+    id: 58,
+    vinoteca: "Los Cortesanos",
+    direccion: "Alvear 281, Rafaela",
+    ir: "https://g.page/LosCortesanos?share",
+    barrio: "Zona Rafaela",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/loscortesanos"
+  },
+  {
+    id: 58,
+    vinoteca: "Sabores del Sol",
+    direccion: "Moreno 348, Rafaela",
+    ir: "https://goo.gl/maps/hZ7agPFKUcGNkg2q7",
+    barrio: "Zona Rafaela",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/rime/saboresdelsol.png"
+  },
+  //Las Rosas
+  {
+    id: 70,
+    vinoteca: "Los Cocos",
+    direccion: "Belgrano 765, Las Rosas",
+    ir: "https://goo.gl/maps/u1QpNe1U7S8MF8kK7",
+    barrio: "Zona Las Rosas",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/random2.jpeg"
+  },
+  //Santa Fe
+  {
+    id: 51,
+    vinoteca: "Don Juan Jacinto",
+    direccion: "Aristóbulo del Valle 6342 - Santa Fe",
+    ir: "https://goo.gl/maps/bLxz4psyonqq7xMJ9",
+    barrio: "Zona Santa Fe",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/donjuanjacinto1"
+  },
+  {
+    id: 52,
+    vinoteca: "Exquisitos Placeres",
+    direccion: "Balcarce 1576 - Santa Fe",
+    ir: "https://goo.gl/maps/pVU8Mw6Bfx6J9NzAA",
+    barrio: "Zona Santa Fe",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/exquisitosplaceres"
+  },
+  {
+    id: 53,
+    vinoteca: "La Cueva de Francia",
+    direccion: "Francia 3490 - Santa Fe",
+    ir: "https://goo.gl/maps/q4eMXN8UEwyswmdo8",
+    barrio: "Zona Santa Fe",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/lacueva"
+  },
+  //Zona Santo Tomé
+    {
+      id: 34,
+      vinoteca: "Entre Copas - Almacén de Vinos",
+      direccion: "Macias 2092 - Santo Tomé",
+      ir: "https://goo.gl/maps/NabVLXmqtG6bFsVi9",
+      barrio: "Zona Santo Tome",
+      bodega: "casarena",
+      thumbnailUrl: "img/vinotecas/casarena/entrecopassantotome"
+  },
+   //Santa Teresa
+   {
+    id: 34,
+    vinoteca: "Cerveteca",
+    direccion: "San Martin 846",
+    ir: "https://goo.gl/maps/i2ay6TzT4En7MY9BA",
+    barrio: "Zona Santa Teresa",
+    bodega: "canale",
+    thumbnailUrl: "img/vinotecas/canale/random2.jpeg"
+  },
+    //Reconquista
+    {
+      id: 7,
+      vinoteca: "La Cava",
+      direccion: "Ituzaingo 1547",
+      ir: "https://goo.gl/maps/RsW64ugCJohNYZqi8",
+      barrio: "Zona Reconquista",
+      bodega: "lagarde",
+      thumbnailUrl: "img/vinotecas/rime/lacava.png"
+    },
+      //Zona Villa Constitucion
+  {
+    id: 15,
+    vinoteca: "Dionisio",
+    direccion: "San Martín 715, Villa Constitución",
+    ir: "https://goo.gl/maps/XT1HtKEK4mU6McbA7",
+    barrio: "Zona Villa Constitucion",
+    bodega: "vinscelto",
+    thumbnailUrl: "img/vinotecas/vinscelto/dionisio.png"
+  },
+  {
+    id: 16,
+    vinoteca: "Entre Copas",
+    direccion: "Dorrego 1101 - Villa Constitución",
+    ir: "https://g.page/entrecopasenoteca?share",
+    barrio: "Zona Villa Constitucion",
+    bodega: "vinscelto",
+    thumbnailUrl: "img/vinotecas/vinscelto/entrecopas"
+  },
+  //Funes y Fisherton
+  {
+    id: 26,
+    vinoteca: "Casa Prada",
+    direccion: "Shopping Fisherton Plaza local 43",
+    ir: "https://goo.gl/maps/NWzZDV1EndumUU5q9",
+    barrio: "Zona Funes y Fisherton",
+    bodega: "casarena",
+    thumbnailUrl: "img/vinotecas/casarena/prada.png"
+  },
+  {
+    id: 52,
+    vinoteca: "MyM Distribuidora",
+    direccion: "Mitre 2599 - Funes",
+    ir: "https://goo.gl/maps/gh9LDqSjwkfCk6Uu5",
+    barrio: "Zona Funes y Fisherton",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/random3.png"
+  },
+  //Roldan
+  {
+    id: 43,
+    vinoteca: "Que buena idea",
+    direccion: "Independencia 234, Roldán",
+    ir: "https://goo.gl/maps/8gseR4nMqT6qhMoB8",
+    barrio: "Zona Roldan",
+    bodega: "canale",
+    thumbnailUrl: "img/vinotecas/canale/quebuenaidea"
+  },
+  //San Nicolás
+  {
+    id: 71,
+    vinoteca: "Vinoteca Ofelia",
+    direccion: "Maipú 17, San Ncolás",
+    ir: "https://goo.gl/maps/TqEpuBtqaLenjPdu7",
+    barrio: "Zona San Nicolas",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/random2.jpeg"
+  },
+  //Esperanza
+  {
+    id: 59,
+    vinoteca: "Los Cortesanos",
+    direccion: "Aarón Castellanos 1802, Esperanza",
+    ir: "https://g.page/VinotecaLosCortesanos?share",
+    barrio: "Zona Esperanza",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/loscortesanosesperanza"
+  },
+  //entre Rios
+  {
+    id: 59,
+    vinoteca: "El Bebedero",
+    direccion: "A. del valle 5381, Gualeguaychu",
+    ir: "https://goo.gl/maps/CUN2jm7fpQbPBti79",
+    barrio: "Zona Entre Rios",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/loscortesanosesperanza"
+  },
+  {
+    id: 59,
+    vinoteca: "Vineyar",
+    direccion: "San Martinb 1320, Lucas Gonzales",
+    ir: "https://goo.gl/maps/jKxXKkuYBEm5fhcZ7",
+    barrio: "Zona Entre Rios",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/rime/random2.jpeg"
+  },
+  {
+    id: 59,
+    vinoteca: "Viejo Tonel",
+    direccion: "Chajarí, Entre Rios",
+    ir: "https://goo.gl/maps/yimk43qPuP1VP4Q57",
+    barrio: "Zona Entre Rios",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/rime/random4"
+  },
+  {
+    id: 59,
+    vinoteca: "Compañía de Vinos",
+    direccion: "San Luis 1222, Concordia,Entre Ríos",
+    ir: "https://goo.gl/maps/tCZCFesyWHLFW4zn7",
+    barrio: "Zona Entre Rios",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/rime/companiadevinos.png"
+  },
+  {
+    id: 59,
+    vinoteca: "Herederos",
+    direccion: "Los Jacarandaes 175, Oro Verde, Entre Ríos",
+    ir: "https://goo.gl/maps/HMqFHy4ZL8cPLRLw7",
+    barrio: "Zona Entre Rios",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/rime/random5.png"
+  },
+  // Las Parejas
+  {
+    id: 44,
+    vinoteca: "Vinoteca ArcoIris",
+    direccion: "Calle 10 num. 1134 - Las Parejas",
+    ir: "https://goo.gl/maps/8M1aCsCvoMseXFHL8",
+    barrio: "Zona Las Parejas",
+    bodega: "casarena",
+    thumbnailUrl: "img/vinotecas/casarena/random1.jpg"
+  },
+  //Paraná
+  {
+    id: 68,
+    vinoteca: "Garay",
+    direccion: "Buenos Aires 481, Paraná",
+    ir: "https://goo.gl/maps/nWcDDD1TiNwk6MQc7",
+    barrio: "Zona Parana",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/random2.jpeg"
+  },
+  //Firmat
+  {
+    id: 68,
+    vinoteca: "Dos Cepas",
+    direccion: "San Martín 1594, Firmat",
+    ir: "https://goo.gl/maps/pJy5p9cw9hKEAgx27",
+    barrio: "Zona Parana",
+    bodega: "amalaya",
+    thumbnailUrl: "img/vinotecas/amalaya/random5.png"
   },
 ]
 
@@ -93,7 +417,7 @@ let vinotecasDelivery = [
     barrio: "Zona Delivery",
     bodega: "mythic",
     thumbnailUrl: "img/vinotecas/mythic/random1.jpg"
-  }
+  },
 ]
 
 selectBarrio.addEventListener('change', () => {
@@ -107,6 +431,9 @@ selectBarrio.addEventListener('change', () => {
       break;
     case "Zona Sur":
       pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Sur'));
+      break;
+    case "Zona Norte":
+      pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Norte'));
       break;
     case "Zona Macro Centro":
       pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Macro Centro'));
@@ -150,9 +477,21 @@ selectBarrio.addEventListener('change', () => {
     case "Zona San Nicolas":
       pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona San Nicolas'));
       break;
+    case "Zona Las Rosas":
+      pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Las Rosas'));
+      break;
+    case "Zona Las Parejas":
+      pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Las Parejas'));
+      break;
+    case "Zona Reconquista":
+      pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Reconquista'));
+      break;
+    case "Zona Firmat":
+      pintarVinotecas(vinotecasRime.filter((el) => el.barrio === 'Zona Firmat'));
+      break;
     case "Zona Delivery":
       Delivery(vinotecasDelivery.filter((el) => el.barrio === 'Zona Delivery'));
-      pintarVinotecas(vinotecaRime.filter((el) => el.barrio === ''));
+      pintarVinotecas(vinotecasRime.filter((el) => el.barrio === ''));
       break;
   }
 })
